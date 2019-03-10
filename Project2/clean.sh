20c6b9a5444bc7319ef393c925d82ea053ed82e9
justinbaskaran <justinbaskaran5@gmail.com>
2019-03-10 14:37:16 -0400

    updated works

--- Project2/clean.sh
+++ Project2/clean.sh
@@ -1,133 +1,125 @@
-#!/bin/bash
+#!/bin/sh
 # Justin Baskaran
 # CS 232- Project 2
 # March 06, 2019
 NumFiles=0
 
 clean_files () {
-shopt -s dotglob
-DIR=$1/*
+#shopt -s dotglob
+#DIR=$1/*
+DIR=$(ls -a $1)
 Inter=""
 Quiet=""
 Stats=""
 for var in "$@"
 do
-    if [[ $var == "i" ]]; then
+    if test $var = "i"; then
         unset Inter
         Inter="i"  
     fi
-    if [[ $var == "q" ]]; then
+    if test $var = "q" ; then
         unset Quiet
         Quiet="q" 
     fi
-    if [[ $var == "s" ]]; then
+
+    if test $var = "s" ; then
         unset Stats
         Stats="s"
 
     fi
 done
-
+#echo "Directory Values: "$DIR
 for file in $DIR; do
-    if [ -d $file ] ; then
-        clean_files $file $Inter $Quiet $Stats $NumFiles
-    else 
-        if [[ $file == *"~" && -e $file ]]; then
-            if [[ $Quiet != "q" ]]; then
-                echo "Deleted File: " $file
-            fi
-            if [[ $Inter == "i" ]]; then
-                echo "Do you want to delete the file (enter y/n)?:  $file"
+    if [ -d $1/$file ] && [ $file != '.' ] && [ $file != '..' ];
+    then
+        #echo "Directory "$1/$file
+        clean_files $1/$file $Inter $Quiet $Stats $NumFiles
+    elif [ $file != '.' ] && [ $file != '..' ] 
+    then
+        #echo "File Found" $file
+
+
+        if [ $1/$file = *"~" ] && [ -e $1/$file]; then
+            toDelete="y"
+            if [ "$Inter" = "i" ]; then
+                echo "Do you want to delete the file (enter y/n)?:  $1/$file"
                 read toDelete
-                if [[ $toDelete == "y" ]]; then
-                    rm $file
-                    let "NumFiles=NumFiles+1"
-                else
-                    echo "Did not delete: $file"
-                    
+            else
+            if test $toDelete = "y"; then
+                if [ "$Quiet" != "q" ]; then
+                    echo "Deleted File: " $1/$file
                 fi
-            else    
-                rm $file
-                let "NumFiles=NumFiles+1"
+                rm $1/$file
+                NumFiles=$((NumFiles+1))
+            fi  
             fi
         fi
         
-        if [[ $file == *"#" && -e $file ]]; then
-            if [[ $Quiet != "q" ]]; then
-                echo "Deleted File: " $file
-            fi
-            if [[ $Inter == "i" ]]; then
-                echo "Do you want to delete the file (enter y/n)?:  $file"
+        if [ $1/$file = *"#" ] && [ -e $1/$file]; then
+            toDelete="y"
+            if [ "$Inter" = "i" ]; then
+                echo "Do you want to delete the file (enter y/n)?:  $1/$file"
                 read toDelete
-                if [[ $toDelete == "y" ]]; then
-                    rm $file
-                    let "NumFiles=NumFiles+1"
-                else
-                    echo "Did not delete: $file"
-                    
+            else
+            if test $toDelete = "y"; then
+                if [ "$Quiet" != "q" ]; then
+                    echo "Deleted File: " $1/$file
                 fi
-            else    
-                rm $file
-                let "NumFiles=NumFiles+1"
+                rm $1/$file
+                NumFiles=$((NumFiles+1))
+            fi  
             fi
         fi
 
-        if [[ $file == *".o" && -e $file ]]; then
-            if [[ $Quiet != "q" ]]; then
-                echo "Deleted File: " $file
-            fi
-            if [[ $Inter == "i" ]]; then
-                echo "Do you want to delete the file (enter y/n)?:  $file"
+        if [ $1/$file = *".o" ] && [ -e $1/$file]; then
+            toDelete="y"
+            if [ "$Inter" = "i" ]; then
+                echo "Do you want to delete the file (enter y/n)?:  $1/$file"
                 read toDelete
-                if [[ $toDelete == "y" ]]; then
-                    rm $file
-                    let "NumFiles=NumFiles+1"
-                else
-                    echo "Did not delete: $file"
-                    
+            else
+            if test $toDelete = "y"; then
+                if [ "$Quiet" != "q" ]; then
+                    echo "Deleted File: " $1/$file
                 fi
-            else    
-                rm $file
-                let "NumFiles=NumFiles+1"
+                rm $1/$file
+                NumFiles=$((NumFiles+1))
+            fi  
             fi
         fi
 
-        if [[ $file != *"."* && $file != *"Makefile"*  && -e $file ]]; then
-            if [[ $Quiet != "q" ]]; then
-                echo "Deleted File: " $file
-            fi
-            if [[ $Inter == "i" ]]; then
-                echo "Do you want to delete the file (enter y/n)?:  $file"
+        if [ $1/$file != *"."* ] && [ $1/$file != *"Makefile" ] && [ -e $1/$file ]; then
+            toDelete="y"
+            # echo "Inter value: "$Inter
+            # echo "Quiet: "$Quiet
+            # echo "toDelete: "$toDelete
+            # echo "Stats: "$Stats
+            if [ "$Inter" = "i" ]; then
+                echo "Do you want to delete the file (enter y/n)?:  $1/$file"
                 read toDelete
-                if [[ $toDelete == "y" ]]; then
-                    rm $file
-                    let "NumFiles=NumFiles+1"
-                else
-                    echo "Did not delete: $file"
-                    
+            else
+            if test $toDelete = "y"; then
+                if [ "$Quiet" != "q" ]; then
+                    echo "Deleted File: " $1/$file
                 fi
-            else    
-                rm $file
-                let "NumFiles=NumFiles+1"
+                rm $1/$file
+                NumFiles=$((NumFiles+1))
+            fi  
             fi
         fi
 
-        if [[ $file == *"core"* && -e $file ]]; then
-            if [[ $Quiet != "q" ]]; then
-                echo "Deleted File: " $file
-            fi
-            if [[ $Inter == "i" ]]; then
-                echo "Do you want to delete the file (enter y/n)?:  $file"
+        if [ $1/$file = "core" ] && [ test -e $1/$file]; then
+            toDelete="y"
+            if [ "$Inter" = "i" ]; then
+                echo "Do you want to delete the file (enter y/n)?:  $1/$file"
                 read toDelete
-                if [[ $toDelete == "y" ]]; then
-                    rm $file
-                    let "NumFiles=NumFiles+1"
-                else
-                    echo "Did not delete: $file"
-                    
+            else
+            if test $toDelete = "y"; then
+                if [ "$Quiet" != "q" ]; then
+                    echo "Deleted File: " $1/$file
                 fi
-            else    
-                rm $file
-                let "NumFiles=NumFiles+1"
+                rm $1/$file
+                NumFiles=$((NumFiles+1))
+            fi  
             fi
         fi
 
@@ -135,39 +127,42 @@ for file in $DIR; do
 done
 }
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    i=""
-    q=""
-    s=""
-    NumFiles=0
-    for var in "$@"
-    do
-        if [[ $var == "-i" || $var == "--interactive" ]]; then
-            unset i
-            i="i"  
-        fi
-        if [[ $var == "-q" || $var == "--quiet" ]]; then
-            unset q
-            q="q" 
-        fi
-        if [[ $var == "-s" || $var == "--stats" ]]; then
-            echo "Size Before: " && du -sh * | grep 20191
-            unset s
-            s="s"
 
-        fi
-    done
-    
-    clean_files $1 $i $q $s $NumFiles
-    echo "Total "$NumFiles
-    
-    if [[ $s == "s" ]]; then
-        echo "Number of Files deleted: " $NumFiles
-        echo "Size After: " && du -sh * | grep 20191
-    fi 
 
-    
-    
-fi
+
+
+i=""
+q=""
+s=""
+
+
+for var in "$@"
+do
+
+    if test $var = "-i" || test $var = "--interactive" ; then
+        unset i
+        i="i"  
+    fi
+    if test $var = "-q" || test $var = "--quiet" ; then
+        unset q
+        q="q" 
+    fi
+    if test $var = "-s" || test $var = "--stats"; then
+        echo "Size Before: " && du -sh * | grep 20191
+        unset s
+        s="s"
+
+    fi
+done
+
+clean_files $1 $i $q $s $NumFiles
+
+if [ "$s" = "s" ]; then
+    echo "Number of Files deleted: " $NumFiles
+    echo "Size After: " && du -sh * | grep 20191
+fi 
+
+
+
 
 
